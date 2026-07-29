faa4a9648a9263165b2f5c6a4cbc3a99a9cb33a2
Logan Benjamin <logan@gyxos.com>
2026-07-30 00:14:12 +1200

    texture and resource allocation working

--- src/main.c
+++ src/main.c
@@ -11,8 +11,10 @@ struct App {
     SDL_Window *window;
     SDL_GPUDevice *gpu_device;
     SDL_GPUResourceSet *resource_set;
-    SDL_GPUTexture *texture;
-    SDL_GPUResourceID resouce;
+    SDL_GPUTexture *texture_1;
+    SDL_GPUResourceID resouce_1;
+    SDL_GPUTexture *texture_2;
+    SDL_GPUResourceID resouce_2;
 };
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
@@ -33,11 +35,38 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
     app->window = SDL_CreateWindow("SDL Bindless Test", 1200, 800, 0);
     app->gpu_device = CreateGPUDevice();
     SDL_ClaimWindowForGPUDevice(app->gpu_device, app->window);
+
     app->resource_set = SDL_CreateGPUResourceSet(app->gpu_device, &(SDL_GPUResourceSetCreateInfo){
         .num_samplers = 8,
         .num_resources = 256,
     });
-    app->texture = SDL_CreateGPUTexture(app->gpu_device, NULL);
+
+    app->texture_1 = SDL_CreateGPUTexture(app->gpu_device, &(SDL_GPUTextureCreateInfo) {
+        .type = SDL_GPU_TEXTURETYPE_2D,
+        .format = SDL_GPU_TEXTUREFORMAT_R8G8B8A8_UNORM,
+        .usage = SDL_GPU_TEXTUREUSAGE_SAMPLER | SDL_GPU_TEXTUREUSAGE_COLOR_TARGET,
+        .width = 256,
+        .height = 256,
+        .layer_count_or_depth = 1,
+        .num_levels = 1,
+        .sample_count = SDL_GPU_SAMPLECOUNT_1,
+        .props = 0,
+    });
+
+    app->texture_2 = SDL_CreateGPUTexture(app->gpu_device, &(SDL_GPUTextureCreateInfo) {
+        .type = SDL_GPU_TEXTURETYPE_2D,
+        .format = SDL_GPU_TEXTUREFORMAT_R8G8B8A8_UNORM,
+        .usage = SDL_GPU_TEXTUREUSAGE_SAMPLER | SDL_GPU_TEXTUREUSAGE_COLOR_TARGET,
+        .width = 256,
+        .height = 256,
+        .layer_count_or_depth = 1,
+        .num_levels = 1,
+        .sample_count = SDL_GPU_SAMPLECOUNT_1,
+        .props = 0,
+    });
+
+    app->resouce_1 = SDL_AllocateGPUResourceTexture(app->gpu_device, app->resource_set, app->texture_1);
+    app->resouce_2 = SDL_AllocateGPUResourceTexture(app->gpu_device, app->resource_set, app->texture_2);
 
     *appstate = app;
 
@@ -64,7 +93,8 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     App *app = appstate;
 
     if (app->gpu_device != NULL) {
-        if (app->texture != NULL) { SDL_ReleaseGPUTexture(app->gpu_device, app->texture); }
+        if (app->texture_1 != NULL) { SDL_ReleaseGPUTexture(app->gpu_device, app->texture_1); }
+        if (app->texture_2 != NULL) { SDL_ReleaseGPUTexture(app->gpu_device, app->texture_2); }
         if (app->resource_set != NULL) { SDL_ReleaseGPUResourceSet(app->gpu_device, app->resource_set); }
         SDL_DestroyGPUDevice(app->gpu_device);
     }
